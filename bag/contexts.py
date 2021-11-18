055f4126a056d648fd018ea0bd261565561683eb
pbaker321 <pete.baker111@gmail.com>
2021-11-18 09:16:46 +0000

    added bag app, contexts and delivery logic

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,8 +1,20 @@
-
+from decimal import Decimal
+from django.conf import settings
 
 def bag_contents(request):
 
-    context = {}
+    bag_items = []
+    total = 0
+    delivery = 10.00
+    product_count = 0
+    grand_total = delivery + total
+
+    context = {
+        'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count,
+        'delivery': delivery,
+        'grand_total': grand_total
+    }
 
     return context
-    
