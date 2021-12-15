560c159c4ccfc8efecc18806b2832253d03846b2
pbaker321 <pete.baker111@gmail.com>
2021-12-15 11:06:06 +0000

    Logged to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn crafty.wsgi:application
--- crafty/settings.py
+++ crafty/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-e81ej!umb$^7r-x=w96976m_=0bwy)mijmu0ut^1i7php@9f@6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['crafty-bear.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -122,12 +122,17 @@ WSGI_APPLICATION = 'crafty.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
