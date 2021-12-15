882277e169ec1806300da19ad703e4ae49062502
pbaker321 <pete.baker111@gmail.com>
2021-12-15 16:03:51 +0000

    Remove Secret key and set debug

--- crafty/settings.py
+++ crafty/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e81ej!umb$^7r-x=w96976m_=0bwy)mijmu0ut^1i7php@9f@6'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['crafty-bear.herokuapp.com', 'localhost']
 
@@ -130,7 +130,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 
