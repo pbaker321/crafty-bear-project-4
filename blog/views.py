707cacaa1f10b7fd7382859381875cc4c9a45b15
pbaker321 <pete.baker111@gmail.com>
2021-12-30 23:41:06 +0000

    Blog functions add, edit and delete

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import (
+    ListView, DetailView,
+    CreateView, UpdateView, DeleteView)
 from .models import Post
+from django.urls import reverse_lazy
 
 
 class BlogView(ListView):
@@ -17,3 +19,15 @@ class AddBlog(CreateView):
     model = Post
     template_name = 'add_blog.html'
     fields = '__all__'
+
+
+class EditBlog(UpdateView):
+    model = Post
+    template_name = 'edit_blog.html'
+    fields = '__all__'
+
+
+class DeleteBlog(DeleteView):
+    model = Post
+    template_name = 'delete_blog.html'
+    success_url = reverse_lazy('blog')
